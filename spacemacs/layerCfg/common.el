922c079c398015d8c02774790934aa156b04877b
renshuo <renshuoo@qq.com>
2020-01-10 03:30:02 +0800

    更新光标形状，行号模式

--- spacemacs/layerCfg/common.el
+++ spacemacs/layerCfg/common.el
@@ -1,5 +1,4 @@
 ;; --------------- common app key binding
-
 ;; for all email client use
 (setq user-mail-address "renshs@aliyun.com")
 (setq user-full-name "Ren Shuo")
@@ -11,16 +10,13 @@
 ;; (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 ;; line number
-(setq-default dotspacemacs-line-numbers 'relative)
+(setq-default dotspacemacs-line-numbers t)
 
 ;;; 闪烁光标
 (blink-cursor-mode t)
 
-;; todo
-;;; 设置光标为竖线（未生效）
-(setq-default cursor-type 'bar)
-;; (setq dotsapcemacs-cursor-type 'bar)
-;;;(setq cursor-type nil)
+;;; 设置光标为竖线
+(setq cursor-type '(bar . 1))
 
 ;; 设置 删除选择的部分， 当有选择内容时，键入任何字符将替换所选部分
 (delete-selection-mode t)
@@ -31,7 +27,6 @@
 (setq-default scroll-step 1
               scroll-margin 3
               scroll-conservatively 10000)
-
 
 ;;标题栏显示 ;; %f 缓冲区完整路径
 (setq frame-title-format "%f")
