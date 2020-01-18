a62d0a23e081b8967ffa22f2b3edf35589c0a1f2
renshuo <renshuoo@qq.com>
2020-01-19 00:21:22 +0800

    增加vue-mode 下的缩进配置

--- spacemacs/mycfg/vue-cfg.el
+++ spacemacs/mycfg/vue-cfg.el
@@ -1,2 +1,10 @@
 (provide 'vue-cfg)
+
+;; for vue template, jscode, css indent
 (setq web-mode-code-indent-offset 2)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-attr-value-indent-offset 2)
+(setq web-mode-attr-indent-offset 2)
+(setq web-mode-sql-indent-offset 2)
+(setq web-mode-style-padding 0)
